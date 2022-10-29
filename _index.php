53bc016efe8f31bf1aef98cbbaf83f376059bf92
gastonos <gastonos@users.noreply.github.com>
2022-10-29 16:52:09 -0300

    #uno

---
+++ _index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>Ruun.org Corremos en Grupo</title>
+<meta charset="UTF-8">
+<meta name="keywords" content="Correr, Run, Community">
+<meta name="description" content="Corremos en Comunidad">
+<meta name="author" content="ruun.org">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+<!--<meta http-equiv="refresh" content="30">-->
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Signika+Negative:wght@600&display=swap" rel="stylesheet">
+
+<style>
+	
+	.container {
+	    position: absolute;
+	    top: 50%;
+	    left: 50%;
+	    -moz-transform: translateX(-50%) translateY(-50%);
+	    -webkit-transform: translateX(-50%) translateY(-50%);
+	    transform: translateX(-50%) translateY(-50%);
+		font-family: 'Signika Negative', sans-serif;
+		font-size:100px;
+	}
+</style>	
+</head>
+
+<body>
+	<div class="container">
+	<span>Ruun.org</span>	
+ 	</div>
+</body>	
+</html>
